27ddae90bdadcf4aaa2b65ff147cdbc357acfc92
Vincent Pietri <vincent.pietri@agence-tbd.com>
2020-12-11 12:09:13 +0100

    SecurityProtocol upgrade to TLS 1.2 & upgrade WSDL from 4.60 to 4.64.1

---
+++ tools/update_api_client_with_wsdl.bat
@@ -0,0 +1,7 @@
+@echo OFF
+set script_directory=%~dp0
+del /f "%script_directory%..\Api_Client\*.cs"
+ 
+%script_directory%wsdl.exe %script_directory%..\wsdl\DirectPaymentAPI.wsdl /n:SDKPaylineDotNet.DirectPaymentAPI /o:%script_directory%..\API_Client\DirectPaymentAPI.cs
+%script_directory%wsdl.exe %script_directory%..\wsdl\WebPaymentAPI.wsdl /n:SDKPaylineDotNet.WebPaymentAPI /o:%script_directory%..\API_Client\WebPaymentAPI.cs
+%script_directory%wsdl.exe %script_directory%..\wsdl\ExtendedAPI.wsdl /n:SDKPaylineDotNet.ExtendedAPI /o:%script_directory%..\API_Client\ExtendedAPI.cs
